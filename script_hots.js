c5e3e65111349735395e8dd9ecd8809f81df7e73
jjslonak3er-source <jjslonak3er@gmail.com>
2026-04-02 11:35:44 -0400

    modified:   script_hots.js

--- script_hots.js
+++ script_hots.js
@@ -424,7 +424,7 @@ if(totalCount > 3){
 let role_score = champ_tank_adjusted + champ_dps_adjusted + champ_support_adjusted + champ_flex_adjusted + champ_offlane_adjusted
 let stat_score = champ_engage_adjusted + champ_peel_adjusted + champ_waveclear_adjusted + champ_sustain_adjusted
 
-let score = (((pairScore + matchScore)**2)/100) * role_score * stat_score * blindable
+let score = (((pairScore + matchScore))/100) * role_score * stat_score * blindable
 
 results.push([champ,score])
 
